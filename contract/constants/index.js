7848cd4b9de72c50ab4d4fb59b9a3adee7a1742a
meharab <meharablatif@hotmail.com>
2022-09-28 22:56:17 +0600

    added script and config and constant/env

---
+++ contract/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "address-of-the-whitelist-contract";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
